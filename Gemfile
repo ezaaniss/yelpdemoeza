a0dcdfdd432d6ddf795418e3c5753fe853441950
ezaaniss <eza.aniss@shareinvestor.com>
2016-07-18 14:19:55 +0800

    Add search functionality

--- Gemfile
+++ Gemfile
@@ -15,6 +15,7 @@ gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem "mini_magick"
 gem 'devise'
+gem 'searchkick'
 group :development, :test do
   gem 'byebug'
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,13 +1,16 @@
+
 class Restaurant < ActiveRecord::Base
-	mount_uploader :image, ImageUploader
+  mount_uploader :image, ImageUploader
+ 
+ searchkick
 
-	has_many :reviews
+  has_many :reviews
 
-	validates :name, :address, :phone, :website, :image, presence: true
-	validates :phone, format: { with: /\A\(\d{3}\) \d{3}-\d{4}\z/,
-  	  message: "must be in the format (123) 456-7890" }
-    validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
-   	  message: "must start with http:// or https://" }
-   	validates :address, format: { with: /\A\d+[^,]+,[^,]+, [A-Z]{2} \d{5}\z/,
-    message: "must be in the format 350 Fifth Avenue, New York NY 11111" }
+  validates :name, :address, :phone, :website, :image, presence: true
+  validates :phone, format: { with: /\A\(\d{3}\) \d{4}-\d{4}\z/,
+    message: "must be in the format (123) 456-7890" }
+  validates :website, format: { with: /\Ahttps?:\/\/.*\z/,
+    message: "must start with http:// or https://" }
+  validates :address, format: { with: /\A\d+, [^,]+, \d{5} [^,]+, [^,]+[A-Z]{2}\z/,
+    message: "must be in the format 30, Jalan PJU Damansara Perdana, 47820 Petaling Jaya, Selangor MY" }
 end
